e6460a488b1c4a7df84fe91ae31c4341d42d9286
ChristianAlexisParedesArroyo <paredes.christian78@outlook.com>
2023-07-19 18:04:55 -0600

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/skills-member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl',
+    scope: {
+      member: '='
+    }
+  };
+}
